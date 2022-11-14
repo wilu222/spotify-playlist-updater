57d2e08cc697292115c198a8cc2848efbf28917c
mousehawk <redjoker.uk@gmail.com>
2022-11-14 22:16:29 +0000

    Check for takedown before updating
    Added if statement to only update the playlist if it has been taken down, so it can run more frequently than every 5 minutes.  
    Added timestamps to record when the playlist has been checked and when it has needed updated.

--- spotify-playlist-updater.py
+++ spotify-playlist-updater.py
@@ -1,12 +1,11 @@
 import time
 import schedule
 import spotipy
+import json
 import spotipy.util as util
 from spotipy.oauth2 import SpotifyOAuth
 
 import base64
-with open("C:\\Users\\You\\YourWorkingDirectory\\Data\\PlaylistPhoto.jpeg", "rb") as img_file:
-    myimage = base64.b64encode(img_file.read())
 
 print("Starting Playlist updater")
   
@@ -14,25 +13,43 @@ def func():
     scope = 'playlist-modify-public ugc-image-upload'
 
     # Spotify Username
-    username = 'username'
+    username = '00000000000'
 
     # Spotify Developer App Client ID and Secret ID
-    SPOTIPY_CLIENT_ID = 'clientid'
-    SPOTIPY_CLIENT_SECRET = 'clientsecret'
+    SPOTIPY_CLIENT_ID = '00000000000000000000000000000000'
+    SPOTIPY_CLIENT_SECRET = '00000000000000000000000000000000'
 
-    token = util.prompt_for_user_token(username,scope,SPOTIPY_CLIENT_ID,SPOTIPY_CLIENT_SECRET,redirect_uri='http://yourdomain.com')
+    token = util.prompt_for_user_token(username,scope,SPOTIPY_CLIENT_ID,SPOTIPY_CLIENT_SECRET,redirect_uri='http://your-url.com')
     sp = spotipy.Spotify(auth=token)
 
     # Playlist Data
-    id = 'https://open.spotify.com/playlist/yourplaylist'
-    playlist_name = 'Playlist Name'
-    playlist_description = 'Playlist Description'
-    image_b64 = myimage
+    id = 'https://open.spotify.com/playlist/0000000000000000000000'
+    results = sp.playlist(id)
+    playlist_name = 'Your Playlist Title'
+    playlist_description = 'Your Playlist Description'
+    
+    # Get the time
+    from datetime import datetime
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
 
-    sp.playlist_upload_cover_image(playlist_id=id, image_b64 = myimage)
-    sp.user_playlist_change_details(username, id, name=playlist_name, description=playlist_description)
+    # Check if playlist has been taken down
+    if playlist_name != (results["name"]):
+        
+        # If yes, update the details
+        with open("C:\\Users\\Work\\Documents\\Tools\\spotify-playlist-updater-main\\data\\ambient.jpg", "rb") as img_file:
+         myimage = base64.b64encode(img_file.read())
+        image_b64 = myimage
+        
+        sp.playlist_upload_cover_image(playlist_id=id, image_b64 = myimage)
+        sp.user_playlist_change_details(username, id, name=playlist_name, description=playlist_description)
+        
+        # Timestamp
+        print(current_time, " - playlist restored")
 
-    print("Playlist updated")
+    else:
+        # If no, do nothing, just print a timestamp
+        print(current_time, " - ok")
 
 func()
 schedule.every(5).minutes.do(func)
